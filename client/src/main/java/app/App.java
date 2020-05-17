1ecd750272b1ac325615741a44520358f79f410e
Nadya <kirillova202001@yandex.ru>
2020-05-17 19:15:51 +0300

    added app in the server

--- client/src/main/java/app/App.java
+++ client/src/main/java/app/App.java
@@ -2,7 +2,6 @@ package app;
 
 import app.Exceptions.InputException;
 import manager.LogManager;
-import server.Server;
 
 public final class App {
     private static final LogManager LOG_MANAGER = LogManager.createDefault(App.class);
@@ -10,9 +9,6 @@ public final class App {
     public static void main(String[] args) {
         Console console = new Console(System.in, System.out);
         LOG_MANAGER.debug("Console was created SUCCESSFULL");
-
-        Server server = new Server(8010, 128);
-        server.start(args);
 
         try {
             LOG_MANAGER.info("App is starting...");
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,85 +3,36 @@ package server;
 import connection.exception.ConnectionException;
 import connectionWorker.ConnectionWorker;
 import controller.Controller;
-import controller.Interpretator;
-import domain.commandsRepository.HistoryRepository;
-import domain.commandsRepository.ICommandsRepository;
 import domain.exception.CreationException;
-import domain.exception.VerifyException;
-import domain.studyGroupFactory.StudyGroupFactory;
-import domain.studyGroupFactory.idProducer.IdProducer;
-import domain.studyGroupRepository.IStudyGroupRepository;
-import domain.studyGroupRepository.TreeSetStudyGroupRepository;
 import manager.LogManager;
 import message.EntityType;
 import message.Message;
 import message.exception.WrongTypeException;
 import query.Query;
 import response.Response;
-import response.ResponseDTO;
 import response.Status;
 import serializer.exception.DeserializationException;
 import serializer.exception.SerializationException;
-import storage.exception.DAOException;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 public class Server {
-    private static final String ARGUMENTS_ERROR = "Введено слишком много аргументов, повторите ввод директории," +
-            " куда будет сохраняться коллекция и сопутсвующие файлы";
     private final int connectionBufferSize;
     private final int port;
+    private final Controller controller;
 
     private static final LogManager LOG_MANAGER = LogManager.createDefault(Server.class);
 
-    public Server(int connectionBufferSize, int port) {
+    public Server(int connectionBufferSize, int port, Controller controller) {
         this.connectionBufferSize = connectionBufferSize;
         this.port = port;
+        this.controller = controller;
     }
 
-    public void start(String[] args) {
-        String pathForAppFiles = null;
-        if (args.length > 0) {
-            checkInputPath(args);
-
-            pathForAppFiles = args[0];
-
-            File file = new File(pathForAppFiles);
-            if (!file.exists()){
-                System.err.println("Такого файла не существует. Проверьте наличие такого файла и повторите попытку.");
-                System.exit(1);
-            }
-
-            if(!file.canExecute()){
-                System.err.println("Недостаточно прав. Пожалуйста, предоставьте права доступа и повторите попытку.");
-                System.exit(1);
-            }
-        }
-
-        IdProducer idProducer = new IdProducer(pathForAppFiles);
-        LOG_MANAGER.debug("IdProducer was created SUCCESSFUL.");
-        StudyGroupFactory studyGroupFactory = new StudyGroupFactory(idProducer);
-        LOG_MANAGER.debug("StudyGroupFactory was created SUCCESFULL.");
-        IStudyGroupRepository studyGroupRepository = null;
-        try {
-            studyGroupRepository = new TreeSetStudyGroupRepository(studyGroupFactory, pathForAppFiles);
-            LOG_MANAGER.debug("IStudyGroupRepository was created SUCCESFULL.");
-        } catch (DAOException | VerifyException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);;
-        }
-
-        ICommandsRepository commandsRepository = new HistoryRepository();
-        LOG_MANAGER.debug("HstoryRepository was created SUCCESSFUL.");
-        Interpretator interpretator = new Interpretator(studyGroupRepository, commandsRepository);
-        LOG_MANAGER.debug("Interpretator was created SUCCESFULL.");
-        Controller controller = new Controller(interpretator, commandsRepository);
-        LOG_MANAGER.debug("Controller was created SUCCESSFUL.");
-
+    public void start() {
         try {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.bind(new InetSocketAddress(port));
@@ -120,12 +71,5 @@ public class Server {
             e.printStackTrace();
         }
 
-    }
-
-    private static void checkInputPath(String[] args) {
-        if (args.length > 1) {
-            System.err.println(ARGUMENTS_ERROR);
-            System.exit(1);
-        }
     }
 }
