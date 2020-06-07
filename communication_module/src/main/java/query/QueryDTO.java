822e3ceb5302d45bd08272e5774467c2bdd7ad6b
Alexey <leshaego14@gmail.com>
2020-06-07 14:33:20 +0300

    Push

--- communication_module/src/main/java/query/QueryDTO.java
+++ communication_module/src/main/java/query/QueryDTO.java
@@ -9,14 +9,12 @@ import java.util.Map;
 public final class QueryDTO extends EntityDTO implements Serializable {
     public String commandName;
     public Map<String, String> arguments;
-    public String accessJWT;
 
     @Override
     public String toString() {
         return "QueryDTO{" +
                 "commandName='" + commandName + '\'' +
                 ", arguments=" + arguments +
-                ", accessJWT='" + accessJWT + '\'' +
                 '}';
     }
 }
--- src/main/java/app/server/App.java
+++ src/main/java/app/server/App.java
@@ -1,22 +1,48 @@
 package app.server;
 
+import app.Console;
+import app.controller.commands.ExitCommand;
+import app.controller.services.exitingDirector.ExitingDirector;
+import app.controller.services.exitingDirector.INeedExiting;
+import controller.ControllerBuilder;
+import controller.command.Command;
+import controller.components.serviceMediator.Service;
+import controller.loginController.LoginCommand;
+import controller.loginController.SignUpCommand;
 import controller.migration.Controller;
 import controller.migration.Interpretator;
+import controller.services.MD2PasswordHashService;
 import domain.commandsRepository.HistoryRepository;
 import domain.commandsRepository.ICommandsRepository;
-import domain.exception.VerifyException;
 import domain.studyGroup.StudyGroup;
-import domain.studyGroupFactory.StudyGroupFactory;
-import domain.studyGroupFactory.idProducer.IdProducer;
 import domain.studyGroupRepository.DBStudyGroupRepository;
 import domain.studyGroupRepository.IStudyGroupRepository;
-import domain.studyGroupRepository.TreeSetStudyGroupRepository;
+import domain.user.User;
+import domain.userRepository.DBUserRepository;
+import domain.userRepository.UserRepository;
 import manager.LogManager;
+import middleware.Middleware;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import router.Router;
+import router.RouterBuilder;
+import router.screen.Screen;
 import server.Server;
+import server.view.Viewer;
+import server.view.screen.MainScreen;
 import storage.dao.DAO;
-import storage.exception.DAOException;
+import storage.postgresDAO.PostgresDAO;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class App {
@@ -24,7 +50,7 @@ public class App {
             " куда будет сохраняться коллекция и сопутсвующие файлы";
     private static final LogManager LOG_MANAGER = LogManager.createDefault(App.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         String pathForAppFiles = null;
         if (args.length > 0) {
             checkInputPath(args);
@@ -43,32 +69,31 @@ public class App {
             }
         }
 
-        IStudyGroupRepository studyGroupRepository = null;
-        try {
-            studyGroupRepository = new TreeSetStudyGroupRepository(studyGroupFactory, pathForAppFiles);
-            LOG_MANAGER.debug("IStudyGroupRepository was created SUCCESFULL.");
-        } catch (DAOException | VerifyException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
-
         File directoryForStoringScripts = new File(pathForAppFiles + "/script");
         if (!directoryForStoringScripts.exists()){
             directoryForStoringScripts.mkdir();
         }
 
-        ICommandsRepository commandsRepository = new HistoryRepository();
-        LOG_MANAGER.debug("HstoryRepository was created SUCCESSFUL.");
-        Interpretator interpretator = new Interpretator(studyGroupRepository, commandsRepository);
-        LOG_MANAGER.debug("Interpretator was created SUCCESFULL.");
-        Controller controller = new Controller(interpretator, commandsRepository);
-        LOG_MANAGER.debug("Controller was created SUCCESSFUL.");
+        Configuration configuration = createConfiguration();
 
-        Server server = new Server(128, 45789, controller);
-        LOG_MANAGER.debug("Server was created SUCCESSFUL.");
+        IStudyGroupRepository studyGroupRepository = createStudyGroupRepository(createStudyGroupDAO());
+        UserRepository userRepository = creaeteUserRepository(creaateUserDAO());
 
-        LOG_MANAGER.info("Server is starting...");
-        server.start();
+        Middleware migrationController = createMigrationController(studyGroupRepository);
+        Middleware loginController = createLoginController(configuration, userRepository);
+
+        ExitingDirector exitingDirector = createExitingDirector();
+        Console console = createConsole();
+        Viewer viewer = createViewer();
+        Screen mainScreen = creaeteMainScreen(configuration, exitingDirector, console, viewer);
+        Router router = createRouter(mainScreen);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        new Server(configuration.getInt("bufferSize"),
+                                   configuration.getInt("port"),
+                                   migrationController,
+                                   executorService,
+                                   router).start();
     }
 
     private static void checkInputPath(String[] args) {
@@ -78,16 +103,97 @@ public class App {
         }
     }
 
-    private static DAO<StudyGroup> createStudyGroupDAO() {
-        
+    private static Configuration createConfiguration() throws ConfigurationException, IOException {
+        String fileName = Paths.get("./config_server.properties").toRealPath().toString();
+
+        Parameters parameters = new Parameters();
+
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(parameters.properties().setFileName(fileName));
+
+        return builder.getConfiguration();
     }
+
+    private static DAO<User> creaateUserDAO() {
+        return new PostgresDAO<>("From User");
+    }
+
+    private static UserRepository creaeteUserRepository(DAO<User> userDAO) {
+        return new DBUserRepository(userDAO);
+    }
+
+    private static DAO<StudyGroup> createStudyGroupDAO() {
+        return new PostgresDAO<>("From StudyGroup");
+    }
+
     private static IStudyGroupRepository createStudyGroupRepository(DAO<StudyGroup> studyGroupDAO) {
-        try {
-            return new DBStudyGroupRepository();
-            LOG_MANAGER.debug("IStudyGroupRepository was created SUCCESFULL.");
-        } catch (DAOException | VerifyException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+        return new DBStudyGroupRepository(studyGroupDAO);
+    }
+
+    private static Middleware createRootMiddleware(Middleware migrationController,
+                                                   Middleware loginController) {
+
+    }
+
+    private static Middleware createMigrationController(IStudyGroupRepository studyGroupRepository) {
+        ICommandsRepository commandsRepository = new HistoryRepository();
+        Interpretator interpretator = new Interpretator(studyGroupRepository, commandsRepository);
+        return new Controller(interpretator, commandsRepository);
+    }
+
+    private static Middleware createLoginController(Configuration configuration,
+                                                    UserRepository userRepository) {
+        Map<String, Class<? extends Command>> commandMap = new HashMap<>();
+        commandMap.put("login", LoginCommand.class);
+        commandMap.put("signUp", SignUpCommand.class);
+
+        Set<Service> services = new HashSet<>();
+        services.add(userRepository);
+        services.add(new MD2PasswordHashService());
+
+        return new ControllerBuilder(configuration, commandMap)
+                        .buildServiceMediator(services)
+                        .build();
+    }
+
+    private static Console createConsole() {
+        return new Console(System.in, System.out);
+    }
+
+    private static Viewer createViewer() {
+        return new Viewer();
+    }
+
+    private static ExitingDirector createExitingDirector() {
+        List<INeedExiting> needExitings = new ArrayList<>();
+        return new ExitingDirector(needExitings);
+    }
+
+    private static Screen creaeteMainScreen(Configuration configuration,
+                                            ExitingDirector exitingDirector,
+                                            Console console,
+                                            Viewer viewer) {
+        Map<String, Class<? extends Command>> commands = new HashMap<>();
+        commands.put("exit", ExitCommand.class);
+
+        Set<Service> services = new HashSet<>();
+        services.add(exitingDirector);
+        services.add(console);
+
+        controller.Controller controller = new ControllerBuilder(configuration, commands)
+                .buildServiceMediator(services)
+                .build();
+
+        MainScreen mainScreen = new MainScreen(console, viewer, controller);
+        exitingDirector.addINeedExiting(mainScreen);
+
+        return mainScreen;
+    }
+
+    private static Router createRouter(Screen mainScreen) {
+        return new RouterBuilder("main")
+                        .registerScreen("main", mainScreen)
+                        .build();
     }
 }
