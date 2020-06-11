fab1b0fd4a291a997a3ca4694bf6b7d0c354b3fc
Yaroslav <yaroslavsoldat2@gmail.com>
2020-06-11 16:58:17 +0300

    qwerty

--- gui/src/main/java/view/main/MainController.java
+++ gui/src/main/java/view/main/MainController.java
@@ -19,18 +19,28 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.FloatStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import javafx.util.converter.LongStringConverter;
 import view.fxController.FXController;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -125,27 +135,27 @@ public class MainController extends FXController implements StudyGroupRepository
         bindColumnsToProductFields();
         initContextMenu();
 
-        /*Localizer.bindComponentToLocale(hasLocationButton, "TableScreen", "availabilityLocation");
-        Localizer.bindComponentToLocale(hasOrganizationButton, "TableScreen", "availabilityOrganization");
-        Localizer.bindComponentToLocale(filter, "TableScreen", "filter");
-        Localizer.bindComponentToLocale(productProps, "TableScreen", "prodProps");
+        /*Localizer.bindComponentToLocale(hasLocationButton, "MainScreen", "availabilityLocation");
+        Localizer.bindComponentToLocale(hasOrganizationButton, "MainScreen", "availabilityOrganization");
+        Localizer.bindComponentToLocale(filter, "MainScreen", "filter");
+        Localizer.bindComponentToLocale(productProps, "MainScreen", "prodProps");
 
-        Localizer.bindComponentToLocale(userIdColumn, "TableScreen", "userId");
-        Localizer.bindComponentToLocale(productNameColumn, "TableScreen", "name");
-        Localizer.bindComponentToLocale(priceColumn, "TableScreen", "price");
-        Localizer.bindComponentToLocale(partNumberColumn, "TableScreen", "partNumber");
-        Localizer.bindComponentToLocale(unitOfMeasureColumn, "TableScreen", "unitOfMeasure");
-        Localizer.bindComponentToLocale(creationDateColumn, "TableScreen", "creationDate");
-        Localizer.bindComponentToLocale(manufactureCostColumn, "TableScreen", "manufactureCost");
-        Localizer.bindComponentToLocale(organizationColumn, "TableScreen", "organization");
-        Localizer.bindComponentToLocale(productColumn, "TableScreen", "product");
-        Localizer.bindComponentToLocale(addressColumn, "TableScreen", "address");
-        Localizer.bindComponentToLocale(locationColumn, "TableScreen", "location");
-        Localizer.bindComponentToLocale(orgNameColumn, "TableScreen", "name");
-        Localizer.bindComponentToLocale(orgAnnualTurnoverColumn, "TableScreen", "anTur");
-        Localizer.bindComponentToLocale(orgTypeColumn, "TableScreen", "type");
-        Localizer.bindComponentToLocale(zipCodeColumn, "TableScreen", "zipCode");
-        Localizer.bindComponentToLocale(coordinatesColumn, "TableScreen", "coordinates");*/
+        Localizer.bindComponentToLocale(userIdColumn, "MainScreen", "userId");
+        Localizer.bindComponentToLocale(productNameColumn, "MainScreen", "name");
+        Localizer.bindComponentToLocale(priceColumn, "MainScreen", "price");
+        Localizer.bindComponentToLocale(partNumberColumn, "MainScreen", "partNumber");
+        Localizer.bindComponentToLocale(unitOfMeasureColumn, "MainScreen", "unitOfMeasure");
+        Localizer.bindComponentToLocale(creationDateColumn, "MainScreen", "creationDate");
+        Localizer.bindComponentToLocale(manufactureCostColumn, "MainScreen", "manufactureCost");
+        Localizer.bindComponentToLocale(organizationColumn, "MainScreen", "organization");
+        Localizer.bindComponentToLocale(productColumn, "MainScreen", "product");
+        Localizer.bindComponentToLocale(addressColumn, "MainScreen", "address");
+        Localizer.bindComponentToLocale(locationColumn, "MainScreen", "location");
+        Localizer.bindComponentToLocale(orgNameColumn, "MainScreen", "name");
+        Localizer.bindComponentToLocale(orgAnnualTurnoverColumn, "MainScreen", "anTur");
+        Localizer.bindComponentToLocale(orgTypeColumn, "MainScreen", "type");
+        Localizer.bindComponentToLocale(zipCodeColumn, "MainScreen", "zipCode");
+        Localizer.bindComponentToLocale(coordinatesColumn, "MainScreen", "coordinates");*/
     }
 
     private <T> StudyGroup getStudyGroup(TableColumn.CellEditEvent<StudyGroup, T> event) {
@@ -163,11 +173,109 @@ public class MainController extends FXController implements StudyGroupRepository
         bindCellsToTextEditors();
     }
 
+    /*private void initMenuBar() {
+        ImageView imageView = new ImageView(new Image(ViewportController.class.getResourceAsStream("/pictures/settings.jpg")));
+        imageView.setFitHeight(17);
+        imageView.setFitWidth(17);
+
+        menu.setGraphic(imageView);
+
+        MenuItem profile = new MenuItem(Localizer.getStringFromBundle("profile", "ViewportScreen"));
+        Localizer.bindComponentToLocale(profile, "ViewportScreen", "profile");
+        profile.setAccelerator(new KeyCodeCombination(KeyCode.P, KeyCodeCombination.CONTROL_DOWN));
+        profile.setOnAction(event -> {
+            Stage newWindow = new Stage();
+
+            FXMLLoader loader = new FXMLLoader();
+            URL url = getClass().getClassLoader().getResource("markup/profile.fxml");
+            loader.setLocation(url);
+
+            ProfileController profileController = new ProfileController(user);
+            loader.setController(profileController);
+
+            Parent parent = null;
+            try {
+                parent = loader.load();
+            } catch (IOException e) {
+                showInternalErrorAlert(Localizer.getStringFromBundle("errorDuling", "ViewportScreen"));
+            }
+
+            newWindow.setScene(new Scene(parent));
+            newWindow.setTitle("Profile");
+            newWindow.show();
+        });
+
+        MenuItem settings = new MenuItem(Localizer.getStringFromBundle("settings", "ViewportScreen"));
+        Localizer.bindComponentToLocale(settings, "ViewportScreen", "settings");
+        settings.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCodeCombination.CONTROL_DOWN));
+        settings.setOnAction(event -> {
+            Stage newWindow = new Stage();
+
+            FXMLLoader loader = new FXMLLoader();
+            URL url = getClass().getClassLoader().getResource("markup/settings.fxml");
+            loader.setLocation(url);
+
+            //SettingsController settingsController = new SettingsController(tableController);
+            //loader.setController(settingsController);
+
+            Parent parent = null;
+            try {
+                parent = loader.load();
+            } catch (IOException e) {
+                //todo todo
+                //showInternalErrorAlert(Localizer.getStringFromBundle("errorDuling", "ViewportScreen"));
+            }
+
+            newWindow.setScene(new Scene(parent));
+            newWindow.setTitle("Profile");
+            newWindow.show();
+        });
+
+        MenuItem logout = new MenuItem(Localizer.getStringFromBundle("logOut", "ViewportScreen"));
+        Localizer.bindComponentToLocale(logout, "ViewportScreen", "logOut");
+        logout.setAccelerator(new KeyCodeCombination(KeyCode.L, KeyCodeCombination.CONTROL_DOWN));
+        logout.setOnAction(event -> {
+            screenContext.remove("accessToken");
+            onStop();
+            LOGGER_ADAPTER.info("All support threads has been stop");
+            screenContext.getRouter().go("signIn");
+        });
+
+        MenuItem refreshCollection = new MenuItem(Localizer.getStringFromBundle("refreshData", "ViewportScreen"));
+        Localizer.bindComponentToLocale(refreshCollection, "ViewportScreen", "refreshData");
+        refreshCollection.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCodeCombination.CONTROL_DOWN));
+        refreshCollection.setOnAction(event -> {
+            try {
+                tableController.change(serverProductDAO.get(new GetAllProductSortedById()));
+            } catch (ServerAdapterException e) {
+                handleServerAdapterException(e);
+            }
+        });
+
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        SeparatorMenuItem separatorMenuItem1 = new SeparatorMenuItem();
+
+        MenuItem exit = new MenuItem(Localizer.getStringFromBundle("exit", "ViewportScreen"));
+        Localizer.bindComponentToLocale(exit, "ViewportScreen", "exit");
+        exit.setAccelerator(new KeyCodeCombination(KeyCode.Q, KeyCodeCombination.CONTROL_DOWN));
+        exit.setOnAction(event -> {
+            try {
+                screenContext.save();
+            } catch (IOException e) {
+                LOGGER_ADAPTER.errorThrowable(e);
+            }
+
+            System.exit(0);
+        });
+
+        menu.getItems().addAll(profile, settings, separatorMenuItem, refreshCollection, separatorMenuItem1, logout, exit);
+    }*/
+
     private void bindCellsToTextEditors() {
         nameCol.setCellFactory(TextFieldTableCell.forTableColumn());
         nameCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, String> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -175,7 +283,7 @@ public class MainController extends FXController implements StudyGroupRepository
                 getStudyGroup(event).setName(event.getNewValue());
             } catch (VerifyException e) {
                 table.refresh();
-                showErrorAlert(Localizer.getStringFromBundle("noteStudyGroup", "TableScreen"));
+                showErrorAlert(Localizer.getStringFromBundle("noteStudyGroup", "MainScreen"));
             }
 
             change(products);
@@ -190,7 +298,7 @@ public class MainController extends FXController implements StudyGroupRepository
         xCoorCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         xCoorCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Integer> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -208,7 +316,7 @@ public class MainController extends FXController implements StudyGroupRepository
         yCoorCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         yCoorCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Integer> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -226,7 +334,7 @@ public class MainController extends FXController implements StudyGroupRepository
         studCountCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         studCountCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Integer> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -244,7 +352,7 @@ public class MainController extends FXController implements StudyGroupRepository
         shouldBeExpCol.setCellFactory(TextFieldTableCell.forTableColumn(new LongStringConverter()));
         shouldBeExpCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Long> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -262,7 +370,7 @@ public class MainController extends FXController implements StudyGroupRepository
         formOfEducCol.setCellFactory(ComboBoxTableCell.forTableColumn(FXCollections.observableArrayList(FormOfEducation.values())));
         formOfEducCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, FormOfEducation> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -281,7 +389,7 @@ public class MainController extends FXController implements StudyGroupRepository
                 .setCellFactory(ComboBoxTableCell.forTableColumn(FXCollections.observableArrayList(Semester.values())));
         semesterCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Semester> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -299,7 +407,7 @@ public class MainController extends FXController implements StudyGroupRepository
         personNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
         personNameCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, String> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -317,7 +425,7 @@ public class MainController extends FXController implements StudyGroupRepository
         heightCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         heightCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Integer> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -335,7 +443,7 @@ public class MainController extends FXController implements StudyGroupRepository
         passportIdCol.setCellFactory(TextFieldTableCell.forTableColumn());
         passportIdCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, String> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -354,7 +462,7 @@ public class MainController extends FXController implements StudyGroupRepository
                 .setCellFactory(ComboBoxTableCell.forTableColumn(FXCollections.observableArrayList(Country.values())));
         natCol.setOnEditCommit((TableColumn.CellEditEvent<StudyGroup, Country> event) -> {
             if (getStudyGroup(event).getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
@@ -379,12 +487,12 @@ public class MainController extends FXController implements StudyGroupRepository
             List<StudyGroup> localList = new ArrayList<>(products);
 
             if (selectedItem == null) {
-                showErrorAlert(Localizer.getStringFromBundle("noteDelete", "TableScreen"));
+                showErrorAlert(Localizer.getStringFromBundle("noteDelete", "MainScreen"));
                 return;
             }
 
             if (selectedItem.getUserId() != user.getId()) {
-                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "TableScreen"));
+                showWarningAlert(Localizer.getStringFromBundle("notYoursDelete", "MainScreen"));
                 return;
             }
 
